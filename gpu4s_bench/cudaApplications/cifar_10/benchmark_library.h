12c395b1e8bb2a22c1f3ae2bbd47e67c84d7de62
Burak Topçu <73446582+topcuburak@users.noreply.github.com>
2023-10-24 02:16:55 -0400

    Add files via upload

---
+++ gpu4s_bench/cudaApplications/cifar_10/benchmark_library.h
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+#ifdef INT
+typedef int bench_t;
+#define __ptype "%d"
+static const std::string type_kernel = "typedef int bench_t;\n";
+#elif FLOAT
+typedef float bench_t;
+#define __ptype "%f"
+static const std::string type_kernel = "typedef float bench_t;\n";
+const float K = 2;
+const float ALPHA = 10e-4;
+const float BETA = 0.75;
+#elif DOUBLE
+typedef double bench_t;
+#define __ptype "%f"
+static const std::string type_kernel = "#pragma OPENCL EXTENSION cl_khr_fp64 : enable\ntypedef double bench_t;\n";
+const double K = 2;
+const double ALPHA = 10e-4;
+const double BETA = 0.75;
+#else 
+	// printf type helper, will resolve to %d or %f given the computed type
+	#define __ptype "%f"
+#endif
+
+#ifdef CUDA
+// CUDA lib
+#include <cuda_runtime.h>
+#elif OPENCL
+// OpenCL lib
+//#include <CL/opencl.h>
+#include <CL/cl.hpp>
+#elif OPENMP
+// OpenMP lib
+#include <omp.h>
+#elif HIP
+// HIP part
+#include <hip/hip_runtime.h>
+#else
+// CPU part
+#endif
+
+
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+struct GraficObject{
+	#ifdef CUDA
+	// CUDA PART
+	bench_t* input_data;
+	bench_t* kernel_1;
+	bench_t* conv_1_output;
+	bench_t* pooling_1_output;
+	bench_t* kernel_2;
+	bench_t* conv_2_output;
+	bench_t* pooling_2_output;
+	bench_t* dense_layer_1_weights;
+	bench_t* dense_layer_1_output;
+	bench_t* dense_layer_2_weights;
+	bench_t* dense_layer_2_output;
+	bench_t* output_data;
+	bench_t* sum_ouput;
+	
+	cudaEvent_t *start_memory_copy_device;
+	cudaEvent_t *stop_memory_copy_device;
+	cudaEvent_t *start_memory_copy_host;
+	cudaEvent_t *stop_memory_copy_host;
+	cudaEvent_t *start;
+	cudaEvent_t *stop;
+   	#elif OPENCL
+   	// OpenCL PART
+	cl::Context *context;
+	cl::CommandQueue *queue;
+	cl::Device default_device;
+	cl::Event *evt_copyIN;
+	cl::Event *evt_copyK1;
+	cl::Event *evt_copyK2;
+	cl::Event *evt_copyW1;
+	cl::Event *evt_copyW2;
+	cl::Event *evt_copyOut;
+	cl::Event *evt1_1;
+	cl::Event *evt1_2;
+	cl::Event *evt1_3;
+	cl::Event *evt1_4;
+	cl::Event *evt2_1;
+	cl::Event *evt2_2;
+	cl::Event *evt2_3;
+	cl::Event *evt2_4;
+	cl::Event *evtd_1;
+	cl::Event *evtd_1_a;
+	cl::Event *evtd_2;
+	cl::Event *evtd_2_a;
+	cl::Event *evt_softmax;
+	cl::Event *evt_softmax_fin;
+
+	cl::Buffer *input_data;
+	cl::Buffer *kernel_1;
+	cl::Buffer *conv_1_output;
+	cl::Buffer *pooling_1_output;
+	cl::Buffer *kernel_2;
+	cl::Buffer *conv_2_output;
+	cl::Buffer *pooling_2_output;
+	cl::Buffer *dense_layer_1_weights;
+	cl::Buffer *dense_layer_1_output;
+	cl::Buffer *dense_layer_2_weights;
+	cl::Buffer *dense_layer_2_output;
+	cl::Buffer *output_data;
+	cl::Buffer *sum_ouput;
+
+	#elif OPENMP
+	// OpenMP part
+	bench_t* input_data;
+	bench_t* kernel_1;
+	bench_t* conv_1_output;
+	bench_t* pooling_1_output;
+	bench_t* kernel_2;
+	bench_t* conv_2_output;
+	bench_t* pooling_2_output;
+	bench_t* dense_layer_1_weights;
+	bench_t* dense_layer_1_output;
+	bench_t* dense_layer_2_weights;
+	bench_t* dense_layer_2_output;
+	bench_t* output_data;
+
+	#elif HIP
+	bench_t* input_data;
+	bench_t* kernel_1;
+	bench_t* conv_1_output;
+	bench_t* pooling_1_output;
+	bench_t* kernel_2;
+	bench_t* conv_2_output;
+	bench_t* pooling_2_output;
+	bench_t* dense_layer_1_weights;
+	bench_t* dense_layer_1_output;
+	bench_t* dense_layer_2_weights;
+	bench_t* dense_layer_2_output;
+	bench_t* output_data;
+	bench_t* sum_ouput;
+	hipEvent_t *start_memory_copy_device;
+	hipEvent_t *stop_memory_copy_device;
+	hipEvent_t *start_memory_copy_host;
+	hipEvent_t *stop_memory_copy_host;
+	hipEvent_t *start;
+	hipEvent_t *stop;
+
+	#else
+	// CPU part
+	bench_t* input_data;
+	bench_t* kernel_1;
+	bench_t* conv_1_output;
+	bench_t* pooling_1_output;
+	bench_t* kernel_2;
+	bench_t* conv_2_output;
+	bench_t* pooling_2_output;
+	bench_t* dense_layer_1_weights;
+	bench_t* dense_layer_1_output;
+	bench_t* dense_layer_2_weights;
+	bench_t* dense_layer_2_output;
+	bench_t* output_data;
+	#endif
+	float elapsed_time;
+};
+
+void init(GraficObject *device_object, char* device_name);
+void init(GraficObject *device_object, int platform, int device, char* device_name);
+bool device_memory_init(GraficObject *device_object, unsigned int input_data, unsigned int output_data, unsigned int kernel_1, unsigned int kernel_2, unsigned int stride_1, unsigned int stride_2, unsigned int neurons_dense_1, unsigned int neurons_dense_2);
+void copy_memory_to_device(GraficObject *device_object, bench_t* input_data, bench_t* kernel_1_data, bench_t* kernel_2_data, bench_t* weights_1 ,bench_t* weights_2,unsigned int input , unsigned int kernel_size_1, unsigned int kernel_size_2, unsigned int weights_1_size, unsigned int weights_2_size);
+void execute_kernel(GraficObject *device_object, unsigned int input_data, unsigned int output_data, unsigned int kernel_1, unsigned int kernel_2, unsigned int stride_1, unsigned int stride_2, unsigned int neurons_dense_1, unsigned int neurons_dense_2);
+void copy_memory_to_host(GraficObject *device_object, bench_t* h_C, int size);
+float get_elapsed_time(GraficObject *device_object, bool csv_format, bool csv_format_timestamp, long int timestamp);
+void clean(GraficObject *device_object);
+
+
+#endif
---
+++ gpu4s_bench/cudaApplications/cifar_10_multiple/main.cpp
@@ -0,0 +1,394 @@
+#include <time.h>
+#include "benchmark_library.h"
+#include "cpu_functions/cpu_functions.h"
+#include <sys/time.h>
+
+#define NUMBER_BASE 1
+#define MIN_VALUE -0.6
+#define MAX_VALUE 0.6
+// OUTPUT C is N x W matrix
+// Print hexadecimal values of result 
+
+#define OK_ARGUMENTS 0
+#define ERROR_ARGUMENTS -1
+
+#define CIFAR_10_INPUT 32
+#define CIFAR_10_OUTPUT 10
+#define KERNEL_CON_1 3
+#define KERNEL_CON_2 3
+#define STRIDE_1 2
+#define STRIDE_2 2
+#define DENSE_1 384
+#define DENSE_2 10
+
+
+#define GPU_FILE "gpu_file.out"
+#define CPU_FILE "cpu_file.out"
+
+int arguments_handler(int argc, char ** argv, BenchmarkParameters* arguments_parameters);
+bench_t RandomNumber();
+
+
+int main(int argc, char *argv[]){
+	// random init
+	//srand (time(NULL));
+	srand (21121993);
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// Arguments  
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	BenchmarkParameters *arguments_parameters = (BenchmarkParameters *)malloc(sizeof(BenchmarkParameters));
+	int resolution = arguments_handler(argc,argv,arguments_parameters);
+	if (resolution == ERROR_ARGUMENTS){
+		exit(-1);
+	}
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// VARIABLES 
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// linearizable versions of matrix
+	unsigned int size_matrix =CIFAR_10_INPUT * CIFAR_10_INPUT * arguments_parameters->size;
+	// A input matrix
+	unsigned int size_A = CIFAR_10_INPUT * CIFAR_10_INPUT * arguments_parameters->size;
+    unsigned int mem_size_A = sizeof(bench_t) * size_A;
+	bench_t* input_data = (bench_t*) malloc(mem_size_A);
+	// B output matrix
+	unsigned int size_B = CIFAR_10_OUTPUT * CIFAR_10_OUTPUT * arguments_parameters->size;
+    unsigned int mem_size_B = sizeof(bench_t) * size_B;
+	bench_t* d_output = (bench_t*) malloc(mem_size_B);
+	// kernel matrix 1
+	unsigned int size_k_1 = KERNEL_CON_1 * KERNEL_CON_2;
+    unsigned int mem_size_k_1 = sizeof(bench_t) * size_k_1;
+	bench_t* kernel_1 = (bench_t*) malloc(mem_size_k_1);
+	// kernel matrix 2
+	unsigned int size_k_2 = KERNEL_CON_2 * KERNEL_CON_2;
+    unsigned int mem_size_k_2 = sizeof(bench_t) * size_k_2;
+	bench_t* kernel_2 = (bench_t*) malloc(mem_size_k_2);
+	// weights  1
+	unsigned int size_w_1 = DENSE_1 * (((CIFAR_10_INPUT / STRIDE_1)/STRIDE_2)*((CIFAR_10_INPUT / STRIDE_1)/STRIDE_2));
+    unsigned int mem_size_w_1 = sizeof(bench_t) * size_w_1;
+	bench_t* weights_1 = (bench_t*) malloc(mem_size_w_1);
+	// weights  1
+	unsigned int size_w_2 = DENSE_1 * DENSE_2;
+    unsigned int mem_size_w_2 = sizeof(bench_t) * size_w_2;
+	bench_t* weights_2 = (bench_t*) malloc(mem_size_w_2);
+	// Outputs 
+	const unsigned int size_pooling_1 = CIFAR_10_INPUT / STRIDE_1;
+    const unsigned int size_pooling_2 = size_pooling_1 / STRIDE_2;
+	bench_t* conv_1_output = (bench_t*) malloc ( CIFAR_10_INPUT * CIFAR_10_INPUT * sizeof(bench_t*));
+	bench_t* pooling_1_output = (bench_t*) malloc ( size_pooling_1 * size_pooling_1 * sizeof(bench_t));
+	bench_t* conv_2_output = (bench_t*) malloc ( size_pooling_1 * size_pooling_1 * sizeof(bench_t*));
+	bench_t* pooling_2_output = (bench_t*) malloc ( size_pooling_2 * size_pooling_2 * sizeof(bench_t));
+   	bench_t* dense_layer_1_output = (bench_t*) malloc ( DENSE_1 * sizeof(bench_t));
+	bench_t* dense_layer_2_output = (bench_t*) malloc ( DENSE_2 * sizeof(bench_t));
+	bench_t* output_data = (bench_t*) malloc ( DENSE_2 * sizeof(bench_t) * arguments_parameters->size);
+	
+	// comparation result
+	bool result = false;
+	// strucs for CPU timing
+	struct timespec start, end;
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// DATA INIT
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	if (strlen(arguments_parameters->input_file_A) == 0)
+	{
+	// inicialice A matrix 
+		for (int k=0; k < arguments_parameters->size; ++k){
+			for (int i=0; i<CIFAR_10_INPUT; ++i){
+		    	for (int j=0; j<CIFAR_10_INPUT; ++j){
+		    		#ifdef INT
+		        	input_data[(CIFAR_10_INPUT * CIFAR_10_INPUT * k) +(i*CIFAR_10_INPUT+j)] = rand() % (NUMBER_BASE * 100);
+
+		        	#else
+		        	input_data[(CIFAR_10_INPUT * CIFAR_10_INPUT * k) +(i*CIFAR_10_INPUT+j)] = (bench_t)rand()/(bench_t)(RAND_MAX/NUMBER_BASE);
+		        	if (arguments_parameters->print_input)
+		        	{
+		        		printf("%f ", input_data[(CIFAR_10_INPUT * CIFAR_10_INPUT * k) +(i*CIFAR_10_INPUT+j)]);
+		        	}
+		        	#endif
+
+		    	}
+		    	if (arguments_parameters->print_input)
+	    		{
+	    			printf("\n");
+	    		}
+			}
+		if (arguments_parameters->print_input)
+	    {
+	    	printf("\n\n");
+	    }
+	  }
+
+	  if (arguments_parameters->print_input)
+	  {
+	    printf("\n");
+	  }
+	// reseed por compasion reasons
+	//srand (time(NULL));
+	srand (21121993);
+	// inicialice kernel 1
+		for (int i=0; i<size_k_1; i++)
+		{
+			#ifdef INT
+	        kernel_1[i] = rand() % (NUMBER_BASE * 100);
+	        #else
+	        kernel_1[i] = RandomNumber();
+	        #endif
+		}
+	// inicialice kernel 2
+		for (int i=0; i<size_k_2; i++)
+		{
+			#ifdef INT
+	        kernel_2[i] = rand() % (NUMBER_BASE * 100);
+	        #else
+	        kernel_2[i] = RandomNumber();
+	        #endif
+		}
+	// inicialice weights 1
+		for (int i=0; i<size_w_1; i++)
+		{
+			#ifdef INT
+	        weights_1[i] = rand() % (NUMBER_BASE * 100);
+	        #else
+	        weights_1[i] = RandomNumber();
+	        #endif
+		}
+	// inicialice weights 2
+		for (int i=0; i<size_w_2; i++)
+		{
+			#ifdef INT
+	        weights_2[i] = rand() % (NUMBER_BASE * 100);
+	        #else
+	        weights_2[i] = RandomNumber();
+	        #endif
+		}
+	// inicialice output
+		for(int i=0; i<size_B; ++i){
+			d_output[i] = 0;
+		}
+	}
+	else
+	{	
+		// load data TODO
+		/*get_double_hexadecimal_values(input_file_A, A,size_A);
+		get_double_hexadecimal_values(input_file_B, B,size_B);
+		
+		// iniciate C matrix
+		for (int i=0; i<size; i++){
+	    	for (int j=0; j<size; j++){
+	        	h_C[i*size+j] = 0;
+	        	d_C[i*size+j] = 0;
+	        	
+	    	}
+		}*/
+	}
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// CODE BENCKMARK
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	// base object init
+	GraficObject *cifar10_bench = (GraficObject *)malloc(sizeof(GraficObject));
+	// init devices
+	char device[100] = "";
+	init(cifar10_bench, 0,arguments_parameters->gpu, device);
+	if (!arguments_parameters->csv_format_timestamp && !arguments_parameters->csv_format && !arguments_parameters->mute_messages ){
+		printf("Using device: %s\n", device);
+	}
+	
+	// init memory
+	bool mem_result = true;
+	mem_result = device_memory_init(cifar10_bench, CIFAR_10_INPUT, CIFAR_10_OUTPUT, KERNEL_CON_1, KERNEL_CON_2, STRIDE_1, STRIDE_2, DENSE_1, DENSE_2, arguments_parameters->size);
+	if (!mem_result)
+	{
+		printf("ERROR MEMORY INIT\n");
+		exit(-1);
+	}
+	// copy memory to device
+	copy_memory_to_device(cifar10_bench, input_data, kernel_1, kernel_2, weights_1, weights_2, CIFAR_10_INPUT, KERNEL_CON_1, KERNEL_CON_2, size_w_1, size_w_2, arguments_parameters->size);
+	// execute kernel
+	execute_kernel(cifar10_bench, CIFAR_10_INPUT, CIFAR_10_OUTPUT, KERNEL_CON_1, KERNEL_CON_2, STRIDE_1, STRIDE_2, DENSE_1, DENSE_2, arguments_parameters->size);
+	// copy memory to host
+	copy_memory_to_host(cifar10_bench, d_output, CIFAR_10_OUTPUT, arguments_parameters->size);
+
+	// get time
+	if (arguments_parameters->print_timing || arguments_parameters->csv_format || arguments_parameters->csv_format_timestamp)
+	{
+		get_elapsed_time(cifar10_bench, arguments_parameters->csv_format, arguments_parameters->csv_format_timestamp, get_timestamp());
+	}
+	if (arguments_parameters->print_output)
+	{
+		#ifdef INT
+		for (int i=0; i < arguments_parameters->size; ++i){
+			for (int j=0; j<CIFAR_10_OUTPUT; ++j){
+	    		printf("%d ", d_output[i*CIFAR_10_OUTPUT + j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+		#else
+		for (int i=0; i < arguments_parameters->size; ++i){
+			for (int j=0; j<CIFAR_10_OUTPUT; ++j){
+	    		printf("%f ", d_output[i*CIFAR_10_OUTPUT + j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+		#endif
+
+
+	}
+	
+
+	if (arguments_parameters->verification)
+	{
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+		cifar10(output_data, conv_1_output, pooling_1_output, conv_2_output, pooling_2_output, dense_layer_1_output, dense_layer_2_output, input_data, kernel_1, kernel_2, weights_1 , weights_2, CIFAR_10_INPUT, CIFAR_10_OUTPUT, KERNEL_CON_1, KERNEL_CON_2, STRIDE_1,STRIDE_2, DENSE_1, DENSE_2, arguments_parameters->size);		
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+		if (arguments_parameters->print_timing)
+		{
+			printf("CPU Time %lu milliseconds\n", (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000);
+		}
+		if (arguments_parameters->print_output)
+		{
+			#ifdef INT
+			for (int i=0; i < arguments_parameters->size; ++i){
+				for (int j=0; j<CIFAR_10_OUTPUT; ++j){
+					printf("%d ", output_data[i*CIFAR_10_OUTPUT + j]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+			#else
+			for (int i=0; i < arguments_parameters->size; ++i){
+				for (int j=0; j<CIFAR_10_OUTPUT; ++j){
+					printf("%f ", output_data[i*CIFAR_10_OUTPUT + j]);
+				}
+				printf("\n");
+			}
+		printf("\n");
+		#endif
+		} 
+	    result = compare_vectors(output_data, d_output, CIFAR_10_OUTPUT);
+	    if (result){
+	    	printf("OK\n");
+	    }
+	    if (arguments_parameters->export_results){
+	    	print_double_hexadecimal_values(GPU_FILE, d_output, CIFAR_10_OUTPUT);
+	    	print_double_hexadecimal_values(CPU_FILE, output_data, CIFAR_10_OUTPUT);
+	    }
+	}
+
+
+	if (arguments_parameters->export_results_gpu)
+	{
+		print_double_hexadecimal_values(GPU_FILE, d_output, size_B);
+	}
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// CLEAN MEMORY
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	// clean device memory
+	clean(cifar10_bench);
+	// free object memory 
+	free(arguments_parameters);
+	free(cifar10_bench);
+	free(input_data);
+	free(d_output);
+	free(kernel_1);
+	free(kernel_2);
+	free(weights_1);
+	free(weights_2);
+	free(conv_1_output);
+	free(pooling_1_output);
+	free(conv_2_output);
+	free(pooling_2_output);
+   	free(dense_layer_1_output);
+	free(dense_layer_2_output);
+	free(output_data);
+return 0;
+}
+
+
+// Arguments part
+
+void print_usage(const char * appName)
+{
+	printf("Usage: %s -s Size -k [-v] [-e] [-o] [-t] [-d] [-i input_file_A_MATRIX input_file_B_MATRIX] \n", appName);
+	printf(" -s Size : set size of x and y of matrices A and B with Size \n");
+	printf(" -e: exports the results of the output and the verification in hexadecimal format (this enables the verification of the results) \n");
+	printf(" -v: verify the output of the gpu program with the cpu output \n");
+	printf(" -g: exports the results of the output \n");
+	printf(" -o: prints the results\n");
+	printf(" -t: prints the timing\n");
+	printf(" -c: prints the timing in csv format\n");
+	printf(" -C: prints the timing in csv format with timestamp\n");
+	printf(" -q: prints input values\n");
+	printf(" -i: pass input data and the result and compares\n");
+	printf(" -d: selects GPU\n");
+	printf(" -x: prints the timing of the validation. Only the sequential time of the application will be displayed\n");
+	printf(" -f: mutes all print\n");
+	printf(" -h: print help information\n");
+}
+
+void init_arguments(BenchmarkParameters* arguments_parameters){
+	arguments_parameters->size = 0;
+	arguments_parameters->gpu = 0;
+	arguments_parameters->verification = false;
+	arguments_parameters->export_results = false;
+	arguments_parameters->export_results_gpu = false;
+	arguments_parameters->print_output = false;
+	arguments_parameters->print_input = false;
+	arguments_parameters->print_timing = false;
+	arguments_parameters->csv_format = false;
+	arguments_parameters->mute_messages = false;
+	arguments_parameters->csv_format_timestamp = false;
+}
+
+int arguments_handler(int argc, char ** argv, BenchmarkParameters* arguments_parameters){
+	init_arguments(arguments_parameters);
+	if (argc == 1){
+		printf("-s need to be set\n\n");
+		print_usage(argv[0]);
+		return ERROR_ARGUMENTS;
+	} 
+	for(unsigned int args = 1; args < argc; ++args)
+	{
+		switch (argv[args][1]) {
+			// comon part
+			case 'v' : arguments_parameters->verification = true;break;
+			case 'e' : arguments_parameters->verification = true; arguments_parameters->export_results= true;break;
+			case 'o' : arguments_parameters->print_output = true;break;
+			case 't' : arguments_parameters->print_timing = true;break;
+			case 'c' : arguments_parameters->csv_format   = true;break;
+			case 'C' : arguments_parameters->csv_format_timestamp = true;break;
+			case 'g' : arguments_parameters->export_results_gpu = true;break;
+			case 'q' : arguments_parameters->print_input = true;break;
+			case 'd' : args +=1; arguments_parameters->gpu = atoi(argv[args]);break;
+			case 'f' : arguments_parameters->mute_messages = true;break;
+					   args +=1;
+					   strcpy(arguments_parameters->input_file_B,argv[args]);
+					   break;
+			// specific
+			case 'i' : args +=1;
+					   strcpy(arguments_parameters->input_file_A,argv[args]);
+					   args +=1;
+					   strcpy(arguments_parameters->input_file_B,argv[args]);
+					   break;
+			case 's' : args +=1; arguments_parameters->size = atoi(argv[args]);break;
+			default: print_usage(argv[0]); return ERROR_ARGUMENTS;
+		}
+
+	}
+	if ( arguments_parameters->size <= 0){
+		printf("-s need to be set\n\n");
+		print_usage(argv[0]);
+		return ERROR_ARGUMENTS;
+	}
+	if (arguments_parameters->mute_messages){
+		arguments_parameters->csv_format = false;
+	}
+	return OK_ARGUMENTS;
+}
+bench_t RandomNumber()
+{
+    return ((bench_t(rand()) / bench_t(RAND_MAX)) * (MAX_VALUE - MIN_VALUE)) + MIN_VALUE;
+}
